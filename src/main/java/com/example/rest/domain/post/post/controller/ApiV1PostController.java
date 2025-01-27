6ef945d6d9685c203fe4dd873a11af89659b6d84
jeonghyeon-ohk <ok6737@naver.com>
2025-01-27 09:25:57 +0900

    modify

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,10 +2,14 @@ package com.example.rest.domain.post.post.controller;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -19,13 +23,38 @@ public class ApiV1PostController {
         return postService.getItems();
     }
 
-    @DeleteMapping("{id}")
-    public String deleteItem(@PathVariable long id) {
+    @DeleteMapping("/{id}")
+    public Map<String, Object> delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
-
         postService.delete(post);
 
-        return "%d번 글 삭제가 완료되었습니다.".formatted(id);
 
+        Map<String, Object> rsData = new HashMap<>();
+        rsData.put("code", "200-1");
+        rsData.put("msg", "%d번 글 삭제가 완료되었습니다.".formatted(id));
+
+        return rsData;
     }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyForm {
+        private String title;
+        private String content;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, Object> modify(@PathVariable long id, @RequestBody ModifyForm form) {
+
+        Post post = postService.getItem(id).get();
+        postService.modify(post, form.getTitle(), form.getContent());
+
+        Map<String, Object> rsData = new HashMap<>();
+        rsData.put("code", "200-1");
+        rsData.put("msg", "%d번 글 수정이 완료되었습니다.".formatted(id));
+
+        return rsData;
+    }
+
+
 }
--- src/main/java/com/example/rest/domain/post/post/service/PostService.java
+++ src/main/java/com/example/rest/domain/post/post/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +18,7 @@ public class PostService {
     public void write(String title, String content) {
 
         postRepository.save(
-                    Post
+                Post
                         .builder()
                         .title(title)
                         .content(content)
@@ -39,5 +40,11 @@ public class PostService {
 
     public void delete(Post post) {
         postRepository.delete(post);
+    }
+
+    @Transactional
+    public void modify(Post post, String title, String content) {
+        post.setTitle(title);
+        post.setContent(content);
     }
 }
