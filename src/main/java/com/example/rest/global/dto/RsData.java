7e842b3712554ab9d12d6c22b47e3238362fdc72
jeonghyeon-ohk <ok6737@naver.com>
2025-01-27 11:47:41 +0900

    jsonInclude option apply

--- src/main/java/com/example/rest/global/dto/RsData.java
+++ src/main/java/com/example/rest/global/dto/RsData.java
@@ -1,10 +1,12 @@
 package com.example.rest.global.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RsData {
     private String code;
     private String msg;
@@ -13,4 +15,5 @@ public class RsData {
     public RsData(String code, String msg) {
         this(code, msg, null);
     }
+
 }
