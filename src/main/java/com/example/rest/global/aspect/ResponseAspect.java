780e252ae9f5397a347ef9858e11e13c14d7c22b
jeonghyeon-ohk <ok6737@naver.com>
2025-01-27 15:42:02 +0900

    aop test

--- src/main/java/com/example/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/example/rest/global/aspect/ResponseAspect.java
@@ -1,11 +1,15 @@
 package com.example.rest.global.aspect;
+
+import com.example.rest.global.dto.RsData;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
 @Aspect
 @Component
 public class ResponseAspect {
+
     @Around("""
             (
                 within
@@ -27,9 +31,18 @@ public class ResponseAspect {
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object test(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("pre");
-        joinPoint.proceed();
-        System.out.println("post");
-        return null;
+        System.out.println("pre"); // 전처리
+
+        Object rst = joinPoint.proceed(); // 실제 수행 메서드
+
+        if(rst instanceof RsData rsData) {
+            String msg = rsData.getMsg();
+            System.out.println("msg : " + msg );
+        }
+
+        System.out.println("post"); // 후처리
+
+        return rst;
     }
+
 }
