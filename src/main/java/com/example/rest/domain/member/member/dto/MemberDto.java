d7377a56dbae3f87e46013b357b8433dd8ca061e
jeonghyeon-ohk <ok6737@naver.com>
2025-01-31 14:08:49 +0900

    DataIntegrityEx

---
+++ src/main/java/com/example/rest/domain/member/member/dto/MemberDto.java
@@ -0,0 +1,28 @@
+package com.example.rest.domain.member.member.dto;
+
+import com.example.rest.domain.member.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MemberDto {
+    private long id;
+    @JsonProperty("createdDatetime")
+    private LocalDateTime createdDate;
+    @JsonProperty("modifiedDatetime")
+    private LocalDateTime modifiedDate;
+    private String username;
+    private String password;
+    private String nickname;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.createdDate = member.getCreatedDate();
+        this.modifiedDate = member.getModifiedDate();
+        this.username = member.getUsername();
+        this.password = member.getPassword();
+        this.nickname = member.getNickname();
+    }
+}
--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.rest.global.exception;
 
 import com.example.rest.global.app.AppConfig;
 import com.example.rest.global.dto.RsData;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -46,6 +47,22 @@ public class GlobalExceptionHandler {
                         new RsData<>(
                                 "400-1",
                                 message
+                        )
+                );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RsData<Void>> DataIntegrityViolationExceptionHandle(DataIntegrityViolationException e) {
+
+        // 개발 모드에서만 작동되도록.
+        if(AppConfig.isNotProd()) e.printStackTrace();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        new RsData<>(
+                                "409-1",
+                                "이미 존재하는 데이터입니다."
                         )
                 );
     }
